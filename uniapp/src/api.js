4828ae67a266349885900ca0d2876faeabda7980
Dpqkqos <168965388+Dpqkqos@users.noreply.github.com>
2025-02-16 23:38:53 +0300

    Update api.js

--- uniapp/src/api.js
+++ uniapp/src/api.js
@@ -1,8 +1,25 @@
-import axios from 'axios';
-const apiClient = axios.create({
-  baseURL: 'https://zany-robot-pj7xj56vxrwq36954-8000.app.github.dev',
-  headers: {
-    'Content-Type': 'application/json',
+import axios from 'axios'
+
+const API_BASE = 'https://zany-robot-pj7xj56vxrwq36954-8000.app.github.dev'
+
+export default {
+  async registerUser(tgId, userData) {
+    return await axios.post(`${API_BASE}/register/${tgId}`, userData)
   },
-});
-export default apiClient;
+
+  async getUser(tgId) {
+    return await axios.get(`${API_BASE}/user/${tgId}`)
+  },
+
+  async createEmotion(tgId, emotionData) {
+    return await axios.post(`${API_BASE}/emotion/${tgId}`, emotionData)
+  },
+
+  async getEmotions(tgId) {
+    return await axios.get(`${API_BASE}/emotions/${tgId}`)
+  },
+
+  async getForecast(tgId) {
+    return await axios.get(`${API_BASE}/forecast/${tgId}`)
+  }
+}
